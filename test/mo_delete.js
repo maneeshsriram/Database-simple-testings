ebadcd281519876cf6e6e787fafb18dd1151a752
Maneesh <maneeshsriram123@gmail.com>
2021-02-01 10:17:22 +0530

    Create mo_delete.js

---
+++ test/mo_delete.js
@@ -0,0 +1,23 @@
+const Student = require('../app/student')
+const assert = require('assert')
+
+describe("Delete test", () => {
+    let deleter
+
+    beforeEach((done) => {
+        deleter = new Student({ name: "Deleter" })
+        deleter.save()
+            .then(() =>
+                done()
+            )
+    })
+
+    it("A delete test", (done) => {
+        Student.findByIdAndDelete(deleter._id)
+            .then(() => Student.findOne({ name: "Deleter" }))
+            .then((student) => {
+                assert(student === null)
+                done()
+            })
+    })
+})
